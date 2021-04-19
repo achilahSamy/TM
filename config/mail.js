f1b6be6f5be0f965b792551ceccff977ba3cd913
sammyA <achilah.jr@gmail.com>
2021-04-19 17:40:56 +0300

    Update mail.js

--- config/mail.js
+++ config/mail.js
@@ -4,7 +4,7 @@ module.exports = {
     host: "mail.twenymoments.com",  // Sender email smtp
     port: "587", // Sender email port
     user: "info@twenymoments.com", // Sender email username
-    pass: "lugambo@18", // Sender email password
+    pass: "pass_word", // Sender email password
 
     // Your email to receive notification (receiver)
     from: '"Contact Me" <info@twenymoments.com>', // Sender email address
